d255346b0a33839f71ae6b65faefd2e06df94232
Candace Johnson <ccj286@nyu.edu>
2019-04-22 20:22:29 -0400

    Initial Commit

---
+++ sprintNYU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sprintNYU
+//
+//  Created by Candace Johnson on 4/22/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
