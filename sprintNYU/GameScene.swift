f9f68ac49e1b31c830ad8d9a57cc8b19f78f6a88
Candace Johnson <Candace@candaces-macbook-pro.local>
2019-04-29 20:09:58 -0400

    added Anisa's files

---
+++ sprintNYU/GameScene.swift
@@ -0,0 +1,60 @@
+//
+//  GameScene.swift
+//  sprintNYU
+//
+//  Created by Anisa Matthews on 4/18/19.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+import SpriteKit
+//import GameplayKit
+
+class GameScene: SKScene {
+    
+    var player = SKSpriteNode(imageNamed: "back0")
+
+    override func didMove(to view: SKView) {
+        /* Setup scene here */
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        backgroundColor = SKColor.white //candace adds bg images
+
+        player.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        player.position = CGPoint(x: 0, y: 0)
+
+        self.addChild(player)
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
