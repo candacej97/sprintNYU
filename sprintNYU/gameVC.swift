ce5e7f1b8d99225b91d62822078e5260e44b5ff2
Candace Johnson <Candace@candaces-macbook-pro.local>
2019-04-29 20:03:38 -0400

    added Anisa's game view controller and game scene files + sprites

---
+++ sprintNYU/gameVC.swift
@@ -0,0 +1,53 @@
+//
+//  GameViewController.swift
+//  sprintNYU
+//
+//  Created by Anisa Matthews on 4/18/19.
+//  Copyright © 2019 nyu.edu. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class gameVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene(size: view.bounds.size)
+        let skView = view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .resizeFill
+        skView.presentScene(scene)
+
+        
+//        if let view = self.view as! SKView? {
+//            // Load the SKScene from 'GameScene.sks'
+//            if let scene = SKScene(fileNamed: "GameScene") {
+//                // Set the scale mode to scale to fit the window
+//                scene.scaleMode = .aspectFill
+//
+//                // Present the scene
+//                view.presentScene(scene)
+//            }
+//
+//            view.ignoresSiblingOrder = true
+//
+//            view.showsFPS = true
+//            view.showsNodeCount = true
+//        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return false
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return false
+    }
+    
+    
+}
